4820bf9ce8fc5a9688dc1443281e70002f81356a
Veniamin <venia@veselovsky.org>
2022-05-22 14:27:54 +0200

    conv2d forward / back

--- Proj_341752_337188_250222/Miniproject_2/model.py
+++ Proj_341752_337188_250222/Miniproject_2/model.py
@@ -2,51 +2,54 @@ import torch
 from torch import nn
 from torch.nn.functional import fold, unfold
 
+torch.set_default_dtype(torch.float64)
 
 class Conv2d():
-    def __init__(self, input_shape, kernel_size, depth, stride):
+    def __init__(self, out_channel, in_channel, kernel_size,stride = 1):
         # Batch size?
-        input_depth, input_height, input_width = input_shape
         self.kernel_size = kernel_size
-        self.depth = depth
-        self.input_shape = input_shape
-        self.input_depth = input_depth
+        self.out_channel = out_channel
         self.stride = stride
-        out_size = lambda s_in, ks, st: int((( s_in - (ks - 1) - 1 ) / st + 1) // 1)
-        self.output_shape = (depth, out_size(input_height, kernel_size, self.stride), out_size(input_width, kernel_size, self.stride))
-        self.kernels_shape = (depth, input_depth, kernel_size, kernel_size)
-        self.kernels = torch.randn(self.kernels_shape)
-        self.biases = torch.randn(depth)
+        self.input_shape = None
+        self.in_channel = in_channel
 
     
     def set_initial(self, weight, bias):
-        self.kernels = weight
-        self.biases = bias
-    
+        self.weight = weight
+        self.bias = bias
+
+    def initialize(self, input):
+        input_height, input_width = input.size()[2:]
+        self.input_shape = (input_height, input_width)
+
+        out_size = lambda s_in, ks, st: int((( s_in - (ks - 1) - 1 ) / st + 1) // 1)
+        self.output_shape = (self.out_channel, out_size(input_height, self.kernel_size, self.stride), out_size(input_width, self.kernel_size, self.stride))
+        self.weight_shape = (self.out_channel, self.in_channel, self.kernel_size, self.kernel_size)
+        self.weight = torch.randn(self.weight_shape)
+        self.bias = torch.randn(self.out_channel)
     
     def forward(self, input):
-        input = input.type(torch.DoubleTensor)
-        print(self.kernels_shape)
+        if self.input_shape == None:
+            self.initialize(input)
+
+        #print(self.weight_shape)
         self.input = input
         # change to empty
-        self.output = torch.clone(self.biases)
+        #print(self.weight)
         
         # do we need to add padding? 
-        inp_unf = unfold(self.input.type(torch.DoubleTensor), kernel_size=self.kernels_shape[-2:], stride=2)
-
-        out_unf = inp_unf.transpose(1, 2).matmul(self.kernels.view(inp_unf.size(1), -1).type(torch.DoubleTensor)).transpose(1, 2)
-
-        self.output = fold(out_unf, output_size=self.output_shape[1:], kernel_size=(1,1), stride=1).permute(0,3,2,1)
-        self.output += self.biases
-        self.output = self.output.permute(0,3,2,1)
+        unfolded = unfold(self.input, kernel_size= (self.kernel_size,self.kernel_size), stride = self.stride )
+        self.output = self.weight.view(self.out_channel, -1) @ unfolded + self.bias.view(1,-1,1)
+        self.output = self.output.view(1,self.out_channel,self.input.shape[2] - self.kernel_size + 1, self.input.shape[3] - self.kernel_size + 1)     
 
         return self.output
     
     def backward(self, output_gradient):
-        ks = self.kernels_shape[-1] - 1
+        x, y = output_gradient.size()[-2:]
+        ks = self.weight_shape[-1] - 1
         self.output_gradient = output_gradient
         # dL/dK = X_j * output_grad
-        inp_dldK = unfold(self.input, kernel_size=self.output_gradient.size()[-2:], stride = 2)
+        inp_dldK = unfold(self.input, kernel_size=self.output_gradient.size()[-2:], stride = self.stride)
         print(self.output_gradient())
         out_dldK = inp_dldK.transpose(1, 2).matmul(self.output_gradient.view(self.output_gradient.size(1) * self.output_gradient.size(2), -1).t()).transpose(1, 2)
         
@@ -55,12 +58,20 @@ class Conv2d():
         # dL/dX_j = sum_i dE/dYi * Kij
         
         
-        self.kernel_flipped = self.kernels.flip([2,3])
+        self.kernel_flipped = self.weight.flip([2,3])
         
-        inp_unf = unfold(output_gradient, kernel_size=self.kernel_flipped[-2:], stride = 2, padding = (self.kernel_size - 1, self.kernel_size - 1))
+        # unstride the output gradient
+        zeros = torch.empty((x -1)* (self.stride -1)+x, y + (y-1)* (self.stride -1)).zero_()
+        zeros[::self.stride,::self.stride] = output_gradient
+        
+        self.unstrided_gradient = zeros
+
+        inp_unf = unfold(self.unstrided_gradient, kernel_size=self.kernel_flipped[-2:], stride = 2, padding = (self.kernel_size - 1, self.kernel_size - 1))
         out_unf = inp_unf.transpose(1, 2).matmul(self.kernel_flipped.view(self.kernel_flipped.size(1) * self.kernel_flipped.size(2), -1).t()).transpose(1, 2)
-        input_gradient = fold(out_unf, output_size=self.kernels_shape[1:], kernel_size=(1, 1), stride=2)
+        input_gradient = fold(out_unf, output_size=self.weight_shape[1:], kernel_size=(1, 1), stride=2)
 
         # Maybe update the weights here??
+        self.weight-= self.learning_rate * out_dldK    
+        self.bias -= output_gradient
 
         return input_gradient
