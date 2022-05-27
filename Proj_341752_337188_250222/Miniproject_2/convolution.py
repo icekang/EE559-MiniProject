bc1579c2c552b06fa2ba8cf419acec7f83c14463
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2022-05-27 22:06:30 +0200

    Cleaning up

--- Proj_341752_337188_250222/Miniproject_2/convolution.py
+++ Proj_341752_337188_250222/Miniproject_2/convolution.py
@@ -14,12 +14,10 @@ class Conv2d(Module):
         self.input_shape = None
         self.in_channel = in_channel
         self.weight_shape = (self.out_channel, self.in_channel, self.kernel_size, self.kernel_size)
-        self.padding_ = padding
+        self.padding = padding
         self.weight = torch.empty(self.weight_shape).uniform_(-1/10,1/10)
-#         self.weight = torch.empty(self.weight_shape).zero_()
 
         self.bias = torch.empty(self.out_channel).zero_()
-#         self.bias = torch.empty(self.out_channel).zero_()
 
         self.weight_grad = torch.empty(self.weight_shape).zero_()
         self.bias_grad = torch.empty(self.out_channel).zero_()
@@ -29,7 +27,7 @@ class Conv2d(Module):
         self.input_shape = (input_height, input_width)
         self.batch_size = input.size(0)
 
-        self.output_shape = (self.out_channel, self.out_size(input_height, self.padding_, self.kernel_size, self.stride), self.out_size(input_width,self.padding_, self.kernel_size, self.stride))
+        self.output_shape = (self.out_channel, self.out_size(input_height, self.padding, self.kernel_size, self.stride), self.out_size(input_width,self.padding, self.kernel_size, self.stride))
 
     def out_size(self, s_in,pad, ks, st):
         return int((( s_in +2*pad - (ks - 1) - 1 ) / st + 1) // 1)
@@ -40,32 +38,31 @@ class Conv2d(Module):
         # if eval == False:
         self.input = input
 
-        unfolded = unfold(input, kernel_size= (self.kernel_size,self.kernel_size), stride = self.stride, padding = self.padding_) 
+        unfolded = unfold(input, kernel_size= (self.kernel_size,self.kernel_size), stride = self.stride, padding = self.padding) 
         if eval == False:
             self.unfolded_forward = unfolded
         output = (self.weight.view(self.out_channel, -1)  @ unfolded) 
         output = output.view(input.size(0),self.out_channel,self.output_shape[1], self.output_shape[2] )+ self.bias.view(self.out_channel,1,1)  
         return output # (B, CO, H',W')
     
-    def backward(self, output_gradient): # (B, co, )
+    def backward(self, gradwrtoutput): # (B, co, )
 
-        B, ci, h, w = self.input.size()
-        B, co, oh, ow = output_gradient.size()
-        ks = self.kernel_size
-        output_gradient = output_gradient 
+        batch_size, input_channel, height, width = self.input.size()
+        batch_size, output_channel, output_height, output_width = gradwrtoutput.size()
         
-        input =  (output_gradient.reshape(B,co,oh*ow).transpose(1,2).reshape(-1,co) @ self.weight.view(co, -1)).view(B, oh*ow, -1).transpose(1,2)
-        input_grad = fold(input, kernel_size = ks, output_size = (h,w),stride = self.stride,padding = self.padding_)
+        rhs = self.weight.view(output_channel, -1)
+        lhs = gradwrtoutput.reshape(batch_size, output_channel, output_height * output_width).transpose(1, 2).reshape(-1, output_channel)
+        input = lhs @ rhs
+        input = input.view(batch_size, output_height*output_width, -1).transpose(1, 2)
+        input_grad = fold(input, kernel_size = self.kernel_size, output_size = (height, width), stride = self.stride, padding = self.padding)
 
-        lhs = output_gradient.view(B, co, -1)
-        rhs = self.unfolded_forward.transpose(1,2)
-        self.weight_grad = lhs.bmm(rhs).sum(dim = 0)
-        self.weight_grad = self.weight_grad.reshape(self.weight_shape) #/ output_gradient.numel()  # (co, ci, ks, ks)  (B,co, output_size, output_size) (B, ci, in_size, in_size)
+        lhs = gradwrtoutput.view(batch_size, output_channel, -1)
+        rhs = self.unfolded_forward.transpose(1, 2)
+        self.weight_grad = lhs.bmm(rhs).sum(dim=0)
+        self.weight_grad = self.weight_grad.reshape(self.weight_shape)
 
-        self.bias_grad = output_gradient.sum((0,2,3)) 
+        self.bias_grad = gradwrtoutput.sum((0,2,3))
 
-#         self.bias_grad = output_gradient.sum((0,2,3))
-        #print("self.bias_grad.mean()",self.bias_grad.mean())
         return input_grad.reshape(self.input.shape)
 
     def param(self):
