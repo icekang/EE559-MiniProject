28bc9d431016a076f59f3bd4ba4cf263b343cfc2
Naravich Chutisilp <namkang_chutisilp@hotmail.com>
2022-06-01 16:54:32 +0200

    MSE gradient calculation was wrong

--- Proj_341752_337188_250222/Miniproject_2/mse.py
+++ Proj_341752_337188_250222/Miniproject_2/mse.py
@@ -6,7 +6,7 @@ class MSE(Module):
 
 	def forward(self, input, target):
 		difference = (input - target)
-		self.grad = 2.0 * (input @ difference)
+		self.grad = 2.0 * (difference)
 		output = difference.pow(2.0)
 		self.grad = self.grad / difference.numel()
 		output = output.mean()
